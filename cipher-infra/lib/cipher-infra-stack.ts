2350ac8ecbac105a9b58983f4c796e93d90f6498
Person Name <mail@mail.com>
2024-11-16 16:44:27 +1100

    Add domain to cloudfront

--- cipher-infra/lib/cipher-infra-stack.ts
+++ cipher-infra/lib/cipher-infra-stack.ts
@@ -4,12 +4,13 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager'; // Add this import
 
 export class CipherProjectsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // VPC setup
+    // VPC setup remains the same
     const vpc = new ec2.Vpc(this, 'CipherVPC', {
       maxAzs: 2,
       natGateways: 1,
@@ -22,13 +23,13 @@ export class CipherProjectsStack extends cdk.Stack {
       ]
     });
 
-    // Deployment bucket for application code
+    // Deployment bucket remains the same
     const deploymentBucket = new s3.Bucket(this, 'DeploymentBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
 
-    // EC2 Role with additional permissions
+    // EC2 role remains the same
     const role = new iam.Role(this, 'EC2Role', {
       assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
       managedPolicies: [
@@ -36,10 +37,9 @@ export class CipherProjectsStack extends cdk.Stack {
       ],
     });
 
-    // Add permission to access deployment bucket
     deploymentBucket.grantRead(role);
 
-    // EC2 Instance
+    // EC2 Instance remains the same
     const instance = new ec2.Instance(this, 'WebServer', {
       vpc,
       vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
@@ -50,22 +50,33 @@ export class CipherProjectsStack extends cdk.Stack {
       role,
     });
 
-    // Allow inbound traffic
-    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+    // Security group rule
+    instance.connections.allowFrom(
+      ec2.Peer.ipv4('130.176.0.0/16'),  // CloudFront IP range
+      ec2.Port.tcp(80)
+    );
 
-    // CloudFront distribution
+    // Certificate (make sure it's in us-east-1 for CloudFront)
+    const certificate = new acm.Certificate(this, 'SiteCertificate', {
+      domainName: 'cipherprojects.com',
+      validation: acm.CertificateValidation.fromDns(),
+    });
+
+    // CloudFront distribution with matching domain
     const distribution = new cloudfront.Distribution(this, 'Distribution', {
       defaultBehavior: {
         origin: new origins.HttpOrigin(instance.instancePublicDnsName),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
+      certificate: certificate,
+      domainNames: ['cipherprojects.com'],  // Match certificate domain
     });
 
-    // Outputs - make sure each has a unique identifier
+    // Outputs remain the same
     new cdk.CfnOutput(this, 'InstanceId', {
       value: instance.instanceId,
     });
-    new cdk.CfnOutput(this, 'DeploymentBucketName', { // This was duplicated
+    new cdk.CfnOutput(this, 'DeploymentBucketName', {
       value: deploymentBucket.bucketName,
     });
     new cdk.CfnOutput(this, 'InstancePublicDNS', {
