e64bc21b90cf22419caa836d4d8d4cb01c7cd96c
Person Name <mail@mail.com>
2024-11-19 01:48:38 +1100

    CDK reset

--- cipher-infra/bin/cipher-infra.ts
+++ cipher-infra/bin/cipher-infra.ts
@@ -1,8 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
-import { CipherInfraStack } from '../lib/cipher-infra-stack';
+import { CipherStack } from '../lib/cipher-infra-stack';
 
 const app = new cdk.App();
-new CipherInfraStack(app, 'CipherInfraStack', {
+new CipherStack(app, 'CipherStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION || 'ap-southeast-2',
--- cipher-infra/lib/cipher-infra-stack.ts
+++ cipher-infra/lib/cipher-infra-stack.ts
@@ -4,19 +4,15 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
-import * as acm from 'aws-cdk-lib/aws-certificatemanager';
-import * as fs from 'fs';
-import * as path from 'path';
 
-export class CipherProjectsStack extends cdk.Stack {
+export class CipherStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    const stackProps = {
+    super(scope, id, {
       ...props,
       crossRegionReferences: true,
-    };
-    super(scope, id, stackProps);
+    });
 
-    // VPC with public and private subnets
+    // VPC
     const vpc = new ec2.Vpc(this, 'CipherVPC', {
       maxAzs: 2,
       natGateways: 1,
@@ -30,35 +26,30 @@ export class CipherProjectsStack extends cdk.Stack {
           cidrMask: 24,
           name: 'Private',
           subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-        },
-      ],
+        }
+      ]
     });
 
-    // Security group for web server
+    // Security Group
     const webServerSG = new ec2.SecurityGroup(this, 'WebServerSG', {
       vpc,
       description: 'Security group for Next.js web server',
       allowAllOutbound: true,
     });
 
-    // Allow HTTP, HTTPS, and SSH traffic
     webServerSG.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(80),
       'Allow HTTP'
     );
+
     webServerSG.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(443),
       'Allow HTTPS'
     );
-    webServerSG.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(22),
-      'Allow SSH'
-    );
 
-    // S3 bucket for deployment artifacts
+    // S3 Bucket
     const deploymentBucket = new s3.Bucket(this, 'DeploymentBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
@@ -66,7 +57,7 @@ export class CipherProjectsStack extends cdk.Stack {
       encryption: s3.BucketEncryption.S3_MANAGED,
     });
 
-    // EC2 IAM role
+    // EC2 Role
     const role = new iam.Role(this, 'EC2Role', {
       assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
       managedPolicies: [
@@ -74,70 +65,45 @@ export class CipherProjectsStack extends cdk.Stack {
       ],
     });
 
-    // Grant read/write access to deployment bucket
-    deploymentBucket.grantReadWrite(role);
+    deploymentBucket.grantRead(role);
 
-    // EC2 instance profile
-    const instanceProfile = new iam.CfnInstanceProfile(this, 'EC2InstanceProfile', {
-      roles: [role.roleName],
-    });
-
-    // User data for EC2 instance
+    // User Data
     const userData = ec2.UserData.forLinux();
     userData.addCommands(
-      `export DEPLOYMENT_BUCKET=${deploymentBucket.bucketName}`,
-      fs.readFileSync(path.join(__dirname, 'user-data.sh'), 'utf8')
+      'yum update -y',
+      'yum install -y nginx',
+      'systemctl start nginx',
+      'systemctl enable nginx'
     );
 
-    // Launch template for EC2 instance
-    const launchTemplate = new ec2.CfnLaunchTemplate(this, 'LaunchTemplate', {
-      launchTemplateData: {
-        instanceType: 't3.micro',
-        imageId: new ec2.AmazonLinuxImage({
-          generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-        }).getImage(this).imageId,
-        securityGroupIds: [webServerSG.securityGroupId],
-        iamInstanceProfile: {
-          arn: instanceProfile.attrArn,
-        },
-        userData: cdk.Fn.base64(userData.render()),
-      },
+    // EC2 Instance
+    const instance = new ec2.Instance(this, 'WebServer', {
+      vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023,
+      }),
+      userData,
+      securityGroup: webServerSG,
+      role,
     });
 
-    // EC2 instance
-    const instance = new ec2.CfnInstance(this, 'WebServerInstance', {
-      launchTemplate: {
-        launchTemplateId: launchTemplate.ref,
-        version: launchTemplate.attrLatestVersionNumber,
-      },
-      subnetId: vpc.publicSubnets[0].subnetId,
-    });
-
-    // CloudFront distribution
-    const certificate = acm.Certificate.fromCertificateArn(this, 'Certificate', 'arn:aws:acm:us-east-1:123456789012:certificate/your-certificate-id');
+    // CloudFront Distribution
     const distribution = new cloudfront.Distribution(this, 'Distribution', {
       defaultBehavior: {
-        origin: new origins.HttpOrigin(instance.attrPublicDnsName, {
+        origin: new origins.HttpOrigin(instance.instancePublicDnsName, {
           protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
         }),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
-      certificate,
-      domainNames: ['yourdomain.com'],
+      minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
     });
 
     // Outputs
-    new cdk.CfnOutput(this, 'DeploymentBucketName', {
-      value: deploymentBucket.bucketName,
-    });
-    new cdk.CfnOutput(this, 'InstanceId', {
-      value: instance.ref,
-    });
-    new cdk.CfnOutput(this, 'InstancePublicDnsName', {
-      value: instance.attrPublicDnsName,
-    });
-    new cdk.CfnOutput(this, 'CloudFrontDomainName', {
-      value: distribution.distributionDomainName,
-    });
+    new cdk.CfnOutput(this, 'InstanceId', { value: instance.instanceId });
+    new cdk.CfnOutput(this, 'DeploymentBucketName', { value: deploymentBucket.bucketName });
+    new cdk.CfnOutput(this, 'InstancePublicDNS', { value: instance.instancePublicDnsName });
+    new cdk.CfnOutput(this, 'DistributionDomainName', { value: distribution.distributionDomainName });
   }
 }
