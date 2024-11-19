aca0a8968c7535870002be4136e88a139ae0067f
Person Name <mail@mail.com>
2024-11-19 17:33:17 +1100

    Post-Reset Push

--- cipher-infra/lib/cipher-infra-stack.ts
+++ cipher-infra/lib/cipher-infra-stack.ts
@@ -4,19 +4,18 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
-import { Construct } from 'constructs';
+import * as fs from 'fs';
+import * as path from 'path';
 
-// Split into two nested stacks
-class InfrastructureStack extends cdk.NestedStack {
-  public readonly vpc: ec2.Vpc;
-  public readonly deploymentBucket: s3.Bucket;
-  public readonly webServerSG: ec2.SecurityGroup;
-  
-  constructor(scope: Construct, id: string, props?: cdk.NestedStackProps) {
-    super(scope, id, props);
+export class CipherStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, {
+      ...props,
+      crossRegionReferences: true,
+    });
 
     // VPC
-    this.vpc = new ec2.Vpc(this, 'CipherVPC', {
+    const vpc = new ec2.Vpc(this, 'CipherVPC', {
       maxAzs: 2,
       natGateways: 1,
       subnetConfiguration: [
@@ -34,38 +33,23 @@ class InfrastructureStack extends cdk.NestedStack {
     });
 
     // Security Group
-    this.webServerSG = new ec2.SecurityGroup(this, 'WebServerSG', {
-      vpc: this.vpc,
+    const webServerSG = new ec2.SecurityGroup(this, 'WebServerSG', {
+      vpc,
       description: 'Security group for Next.js web server',
       allowAllOutbound: true,
     });
 
-    this.webServerSG.addIngressRule(
+    webServerSG.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(80),
       'Allow HTTP'
     );
 
-    this.webServerSG.addIngressRule(
+    webServerSG.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(443),
       'Allow HTTPS'
     );
-
-    // Deployment bucket
-    this.deploymentBucket = new s3.Bucket(this, 'DeploymentBucket', {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      versioned: true,
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      bucketName: `cipher-deployment-${cdk.Stack.of(this).region}-${cdk.Stack.of(this).account}`,
-    });
-  }
-}
-
-class WebServerStack extends cdk.NestedStack {
-  constructor(scope: Construct, id: string, infrastructure: InfrastructureStack, props?: cdk.NestedStackProps) {
-    super(scope, id, props);
 
     // EC2 Role
     const role = new iam.Role(this, 'EC2Role', {
@@ -75,26 +59,37 @@ class WebServerStack extends cdk.NestedStack {
       ],
     });
 
-    infrastructure.deploymentBucket.grantRead(role);
+    // Deployment Bucket
+    const deploymentBucket = new s3.Bucket(this, 'DeploymentBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: true,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      bucketName: `cipher-deployment-${this.region}-${this.account}`,
+    });
+    deploymentBucket.grantRead(role);
 
     // User Data
     const userData = ec2.UserData.forLinux();
     userData.addCommands(
       `# Version: ${Date.now()}`,
-      `export DEPLOYMENT_BUCKET="${infrastructure.deploymentBucket.bucketName}"`,
+      // Add explicit logging of the bucket name
+      `echo "Setting up deployment bucket name..."`,
+      `export DEPLOYMENT_BUCKET="${deploymentBucket.bucketName}"`,
+      `echo "Deployment bucket set to: $DEPLOYMENT_BUCKET"`,
       fs.readFileSync(path.join(__dirname, 'user-data.sh'), 'utf8')
     );
 
     // EC2 Instance
     const instance = new ec2.Instance(this, 'WebServer', {
-      vpc: infrastructure.vpc,
+      vpc,
       vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
       machineImage: new ec2.AmazonLinuxImage({
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023,
       }),
       userData,
-      securityGroup: infrastructure.webServerSG,
+      securityGroup: webServerSG,
       role,
     });
 
@@ -102,33 +97,21 @@ class WebServerStack extends cdk.NestedStack {
     const distribution = new cloudfront.Distribution(this, 'Distribution', {
       defaultBehavior: {
         origin: new origins.HttpOrigin(instance.instancePublicDnsName, {
-          protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+          protocolPolicy: cloudfront.OriginProtocolPolicy.HTTPS_ONLY,
         }),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
       minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
     });
+    
+    distribution.addBehavior('/static/*', new origins.S3Origin(deploymentBucket), {
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    });
 
     // Outputs
     new cdk.CfnOutput(this, 'InstanceId', { value: instance.instanceId });
+    new cdk.CfnOutput(this, 'DeploymentBucketName', { value: deploymentBucket.bucketName });
     new cdk.CfnOutput(this, 'InstancePublicDNS', { value: instance.instancePublicDnsName });
     new cdk.CfnOutput(this, 'DistributionDomainName', { value: distribution.distributionDomainName });
-  }
-}
-
-export class CipherStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // Create infrastructure first
-    const infrastructure = new InfrastructureStack(this, 'Infrastructure');
-    
-    // Create web server stack that depends on infrastructure
-    const webServer = new WebServerStack(this, 'WebServer', infrastructure);
-
-    // Output the bucket name for GitHub Actions
-    new cdk.CfnOutput(this, 'DeploymentBucketName', { 
-      value: infrastructure.deploymentBucket.bucketName 
-    });
   }
 }
