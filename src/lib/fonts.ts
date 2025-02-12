3c0aeefce9236694689c16d73b3f13d7b572aa70
Keith <keith.vaughan@cipherprojects.com>
2025-02-12 12:59:52 +1100

    Fixed font loading

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,9 +3,44 @@ import localFont from 'next/font/local'
 export const neueHaas = localFont({
   src: [
     {
+      path: '../../public/fonts/NeueHaasDisplay-XXThin.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/NeueHaasDisplay-XXThinItalic.woff2',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/NeueHaasDisplay-XThin.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/NeueHaasDisplay-XThinItalic.woff2',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/NeueHaasDisplay-Thin.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/NeueHaasDisplay-ThinItalic.woff2',
+      weight: '300',
+      style: 'italic',
+    },
+    {
       path: '../../public/fonts/NeueHaasDisplay-Roman.woff2',
       weight: '400',
       style: 'normal',
+    },
+    {
+      path: '../../public/fonts/NeueHaasDisplay-RomanItalic.woff2',
+      weight: '400',
+      style: 'italic',
     },
     {
       path: '../../public/fonts/NeueHaasDisplay-Medium.woff2',
@@ -13,14 +48,33 @@ export const neueHaas = localFont({
       style: 'normal',
     },
     {
+      path: '../../public/fonts/NeueHaasDisplay-MediumItalic.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+    {
       path: '../../public/fonts/NeueHaasDisplay-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
+    {
+      path: '../../public/fonts/NeueHaasDisplay-BoldItalic.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/NeueHaasDisplay-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/NeueHaasDisplay-BlackItalic.woff2',
+      weight: '900',
+      style: 'italic',
+    },
   ],
   display: 'swap',
   preload: true,
-  // Subset the font to only include Latin characters
   variable: '--font-neue-haas',
-  fallback: ['system-ui', 'arial'],
+  fallback: ['system-ui', 'arial']
 })
